97d1a7a4b110955b664590bb0e810f3c60403c96
23103400 <23103400@MAIL.JIIT.AC.IN>
2025-08-05 21:38:02 +0530

    Update week0.cpp

--- week0.cpp
+++ week0.cpp
@@ -5,14 +5,19 @@ using namespace std;
 int main() {
     vector<int>arr;
     int sum=0;
-    int avg=0;
-    for(int i =0;i<arr.size();i++){
+    int n ;
+    cin>>n;
+    for(int i =0;i<n;i++){
         int val;
         cin>>val;
-        arr.pushback(val);
+       arr.push_back(val);
         sum+=val;
     }
-    avg=sum/arr.size();
-    cout<<avg
+  
+    int avg= sum/n;
+    cout<<avg;
+    
+    
     return 0;
 }
+
